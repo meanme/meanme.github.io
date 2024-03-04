e70fe621bca1adc18620f2378483ff5021dfc912
Matteo <matteo@meanme.com>
2024-03-04 01:05:54 -0800

    Revert config changes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'meanme.github.io'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
 const { withContentlayer } = require("next-contentlayer")
 
 const nextConfig = {
     output: "export",
-    assetPrefix,
-    basePath,
     reactStrictMode: true,
     compiler: {
         removeConsole: true,
