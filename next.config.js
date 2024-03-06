3917565d6b63bdc7d53f7f474f3ed1f59328e72f
Matteo <matteo@meanme.com>
2024-03-05 21:58:36 -0800

    Added base path

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { withContentlayer } = require("next-contentlayer")
 
 const nextConfig = {
     output: "export",
+    basePath: "/meanme.github.io",
     reactStrictMode: true,
     compiler: {
         removeConsole: true,
